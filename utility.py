fc78a498742815970f5e950ddf70bee97d49356f
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2022-05-06 08:38:11 +0100

    minor fixes

--- utility.py
+++ utility.py
@@ -8,8 +8,10 @@ from weightedlist import WeightedList
 
 def display(wl: WeightedList, /, *, rarity = True) -> str:
   '''Print the contents of a `WeightedList` in an inventory-like fashion.'''
+
   return "\n".join(f"x{i.weight} {repr(i.value)}{f'  ({round(100 * i.weight / len(wl), 2)}%)' * rarity}" for i in wl)
 
 def rarity(wl: WeightedList, /, *, dp = 4) -> str:
   '''Print the rarities (selection probabilities) of each item in a `WeightedList`.'''
+  
   return {i: round(i.weight / len(wl), dp) for i in wl}
