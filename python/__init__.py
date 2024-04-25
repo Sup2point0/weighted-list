b703c1db2d9adf4c57b4bbdf9266c61fd68dcc38
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-25 09:00:02 +0000

    restructure `__init__.py`s

---
+++ python/__init__.py
@@ -0,0 +1,11 @@
+'''
+weighted-list
+v2.0.0
+
+Lists of weighted items for weighted selection
+by Sup#2.0 (@Sup2point0)
+
+Available at <https://github.com/Sup2point0/weighted-list>
+'''
+
+from .source import *
--- python/source/__init__.py
+++ python/source/__init__.py
@@ -1,12 +1,3 @@
-'''
-weighted-list
-v2.0.0
-
-Lists of weighted items for weighted selection
-by Sup#2.0 (@Sup2point0)
-
-Available at <https://github.com/Sup2point0/weighted-list
-'''
-
-from .weighted_list import WeightedList
-from .frozen_weighted_list import FrozenWeightedList
+from .weightedlist import WeightedList
+# from .weighted_list import WeightedList
+# from .frozen_weighted_list import FrozenWeightedList
