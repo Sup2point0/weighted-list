2cae056e7c10fdc5b8c7d14d80b7ccf79f188f82
Sup#2.0 <sup2point0@gmail.com>
2025-11-04 12:46:35 +0000

    hs - setup tests

--- haskell/tests/Test_WeightedList.hs
+++ haskell/tests/Test_WeightedList.hs
@@ -1,8 +1,9 @@
-module TestMatrix where
+module Test_WeightedList where
 
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import WeightedList
 import Syntax
 
 
@@ -10,6 +11,16 @@ test_weighted_list = testGroup "WeightedList"
   [ test_collection "constructor" test_constructor
   ] :: TestTree
 
+
+t = newWeightedList
+  [(2, "sup"), (3, "nova"), (7, "shard")]
+  :: WeightedList String Int
+
+
 test_constructor =
-  [ newWeightedList [] === []
+  [ t ===
+    [ WeightedItem { value = "sup", weight = 2 }
+    , WeightedItem { value = "nova", weight = 3 }
+    , WeightedItem { value = "shard", weight = 7 }
+    ]
   ] :: [Assertion]
