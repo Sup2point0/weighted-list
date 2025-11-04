034fce08a7c27695d07174c7410ba4cb24bce1aa
Sup#2.0 <sup2point0@gmail.com>
2025-11-04 16:45:07 +0000

    hs - add tests

--- haskell/tests/Test_WeightedList.hs
+++ haskell/tests/Test_WeightedList.hs
@@ -3,24 +3,60 @@ module Test_WeightedList where
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import WeightedList
+import Data.Tuple
+
 import Syntax
+import WeightedList
 
 
 test_weighted_list = testGroup "WeightedList"
   [ test_collection "constructor" test_constructor
+  , test_collection "properties" test_properties
+  , test_collection "indexing" test_indexing
   ] :: TestTree
 
 
-t = newWeightedList
-  [(2, "sup"), (3, "nova"), (7, "shard")]
+l  = [(2, "sup"), (3, "nova"), (7, "shard")]
+wl = newWeightedList l
   :: WeightedList String Int
 
 
 test_constructor =
-  [ t ===
-    [ WeightedItem { value = "sup", weight = 2 }
-    , WeightedItem { value = "nova", weight = 3 }
-    , WeightedItem { value = "shard", weight = 7 }
-    ]
+  [
+    newWeightedList @String @Int [] === []
+
+  , wl === [ WeightedItem { value = "sup", weight = 2 }
+           , WeightedItem { value = "nova", weight = 3 }
+           , WeightedItem { value = "shard", weight = 7 }
+           ]
+  
+  ] :: [Assertion]
+
+
+test_properties =
+  [
+    total_values wl === 3
+  , total_weights wl === 12
+  , values wl === ["sup", "nova", "shard"]
+  , weights wl === [2, 3, 7]
+  , raw wl === l
+  , raw' wl === map swap l
+  ]
+
+
+test_indexing =
+  [
+    value (get wl 0)  === "sup"
+  , value (get wl 1)  === "sup"
+  , value (get wl 2)  === "nova"
+  , value (get wl 3)  === "nova"
+  , value (get wl 4)  === "nova"
+  , value (get wl 5)  === "shard"
+  , value (get wl 6)  === "shard"
+  , value (get wl 7)  === "shard"
+  , value (get wl 8)  === "shard"
+  , value (get wl 9)  === "shard"
+  , value (get wl 10) === "shard"
+  , value (get wl 11) === "shard"
+  
   ] :: [Assertion]
