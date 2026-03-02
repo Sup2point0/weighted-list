6b963aab95d1c3e56d13dc11ba608fe5b65c6c9e
Sup#2.0 <sup2point0@gmail.com>
2026-03-02 11:58:45 +0000

    ts - add tests

--- typescript/tests/fwlist/constructors.test.ts
+++ typescript/tests/fwlist/constructors.test.ts
@@ -4,23 +4,43 @@ import { FWList } from "../../weighted-list";
 import { fwl } from "./shared";
 
 
-test("constructors", () =>
+test("from-WeightedItems", () =>
 {
   assert.deepEqual( fwl(), new FWList(
     { weight: 2, value: "sup"   },
     { weight: 3, value: "nova"  },
     { weight: 5, value: "shard" },
   ));
-  
+});
+
+test("from-FrozenWeightedItems", () =>
+{
   assert.deepEqual( fwl(), new FWList(
     { weight: 2, value: "sup",   cumulative_weight: 0 },
     { weight: 3, value: "nova",  cumulative_weight: 0 },
     { weight: 5, value: "shard", cumulative_weight: 0 },
   ));
+});
 
+test("incorrect-order", () =>
+{
   assert.throws(() => new FWList(
     // if IDE is giving an error, good - weight should come first!
     ["sup", 2]
+  ));
+});
+
+test("zero-weight", () =>
+{
+  assert.throws(() => new FWList(
+    [0, "aleph"]
+  ));
+});
+
+test("negative-weight", () =>
+{
+  assert.throws(() => new FWList(
+    [-1, "eto"]
   ));
 });
 
